6454fd86091fbe312f74ffadfbc0b4c2dd4610da
Rodion Kryuchkov <rod6588@gmail.com>
2016-01-29 23:24:52 +0200

    added favicon

--- application/views/header.php
+++ application/views/header.php
@@ -9,6 +9,7 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
         <!-- Custom CSS -->
         <link rel="stylesheet" href="<?php echo assets_url() . 'css/style.css' ?>">
+        <link rel="icon" type="image/png" href="<?php echo assets_url() . 'img/favicon.ico' ?>">
     </head>
     <body>
 
